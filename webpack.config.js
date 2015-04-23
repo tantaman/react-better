6226d297d5d73dce8df71027f7f5141e2515a318
Matthew Crinklaw-Vogt <mlaw@fb.com>
2015-04-22 19:37:41 -0700

    initial config

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  entry: './src/scripts/main',
+  output: {
+    path: __dirname + '/dist/scripts',
+    filename: 'main.js'
+  },
+  module: {
+    loaders: [
+      { test: /\.css$/, loader: 'style!css' },
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader?optional=runtime'}
+    ]
+  }
+};
